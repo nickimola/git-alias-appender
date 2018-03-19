75b3cada77de1a7a5d7df980fd200cd1fb4b057c
Nicholas Lazzerini <nicholas.lazzerini.external@ynap.com>
2018-03-19 13:11:26 +0100

    Adjusted logs to be more readable

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ const oldConfig = ini.parse(fs.readFileSync(gitconfigPath, 'utf-8'))
 
 oldConfig.alias = newConfig.alias
 
-console.log("appending new aliases to .gitconfig file...");
+console.log("appending new aliases to .gitconfig file.......");
 
 fs.writeFileSync(gitconfigPath, ini.stringify(oldConfig));
 
 console.log("Done!");
-console.log("----------------------");
+console.log("----------------------------------");
+console.log("----------------------------------");
 console.log("To be able to use the newly added aliases, please restart your console.");
+console.log("----------------------------------");
+console.log("----------------------------------");
